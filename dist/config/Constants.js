3e3770e3f8420b9adb5a0ad07ab562220ed0f2b2
johnnynavarro <johnnynavarro@gmail.com>
2021-10-24 04:23:24 -0300

    Api-shortner

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://userdio:0Pn2d3hHgiXd32K7@urlshortner-dionavarro.ylf7h.mongodb.net/urlshortner-dionavarro?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://userdio:0Pn2d3hHgiXd32K7@urlshortner-dionavarro.ylf7h.mongodb.net/urlshortner-dionavarro?retryWrites=true&w=majority',
+}
+
